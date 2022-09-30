41b476d4db81c068639aa658f6d6d440eb48ad06
awa <awasoumbane281@.com>
2022-09-30 12:30:20 +0000

    new

--- views/index.php
+++ views/index.php
@@ -12,9 +12,40 @@
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"
         integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
+<header class="header">
+   <div class="haut">
+              <img src="images/logo.png" alt="logo" id="logo">
+        
+                <a href="" ><i class="fa-solid fa-house"></i>Acceuil</a>
+                <a href=""><i class="fa-solid fa-user"></i>Se connecter</a>
+        
+                </div>
+   
+</header>
+<section>
+    <div>
+        <img src="images/prem3.jpg" alt="image">
+    </div>
+    <div>
+         <h1>
+            <b>Description du projet</b>
+         </h1>
+          <p>
+    L’école de la réussite est un établissement d’enseignement privé mixte (élémentaire et secondaire) avec une réputation qui l’a précède faisant d’elle une cible prioritaire à tous ceux qui souhaitent bénéficier d’une formation de qualité. 
+    Les parents d’élèves étant conscient de la situation font converger leurs enfants dans ladite école pour leur faire bénéficier d’une éducation de base leurs permettant d’asseoir au maximum leur niveau.
+     Au cours de l’année académique 2021/2022 l’effectif de l'école a dépassé les 10 000 écoliers.
+      Face à cette montée exponentielle du nombre d'inscrits, l’administration se sent véritablement débordée dans la gestion et décide de digitaliser son système.   
+         </p>
+    </div>
+   
+</section>
+<footer>
+<p>@2D2S 2022</p>
+</footer>
 
 </body>
 
