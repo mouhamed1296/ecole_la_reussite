a849f9ed3b117e3c742684ba1f2be8ecd70c0a6a
ODIA221 <senbusinessfk@gmail.com>
2022-09-30 14:29:06 +0200

    premier commit

---
+++ views/dashboardAdmin.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashbord</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"
+        integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <link rel="stylesheet" href="css/dashboardAdmin.css">
+        
+</head>
+
+<body>
+    <!---->
+    <footer>
+        <div id="Entete">
+            <i class="fa-sharp fa-solid fa-bars"></i>
+
+            <br>
+            <br>
+
+            <h2>Tableau de bord</h2>
+
+                <br>
+                <br>
+
+                <ul id="nav_menu">
+                    <div class="item">
+                        <li>
+                            <a href="#" > Ajouter Emploué</a>
+                        </li>
+                    </div>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > Modifier Employé</a>
+                        </li>
+                    </div>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > Archiver Employé</a>
+                        </li>
+                    </div>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > Lister Employé</a>
+                        </li>
+                    </div>
+
+                    <br>
+                    <br>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > Ajouter ÉLéve</a>
+                        </li>
+                    </div>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > Modifier ÉLéve</a>
+                        </li>
+                    </div>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > Archiver ÉLéve</a>
+                        </li>
+                    </div>
+
+                    <div class="item">
+                        <li>
+                            <a href="#" > lister ÉLéve</a>
+                        </li>
+                    </div>
+                </ul>
+
+                <br>
+                <br>
+
+                <button type="submit">Se Déconnecter</button>
+
+
+        </div>
+
+    </footer>
+    <!---->
+    <mian>
+
+    </main>
+    <!---->
+    <footer>
+
+    </footer>
+    <!---->
+   
+
+</body>
+
+</html>
