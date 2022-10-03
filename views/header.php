0a63830b612b7455a3c51b395ca2d30930dfb7a9
Mamadou Sarr <mbayelel@gmail.com>
2022-10-03 16:56:40 +0000

    reponsive et getion des erreurs de connexion

--- views/header.php
+++ views/header.php
@@ -1,12 +1,18 @@
-
 <header class="header">
-   
-   <img src="images/logo.png" alt="logo" id="logo">
-<ul class="acceuil">
- 
-     <a href="/projet_gestion_ecole/acceuil"> <i class="fa-solid fa-house"></i> Acceuil</a>
-     <a href="/projet_gestion_ecole/connexion"><i class="fa-solid fa-user"></i> Se connecter</a>
-     
-</ul>
+    <img src="images/logo.png" alt="logo" id="logo">
+    <ul class="acceuil">
+        <li>
+            <a href="/projet_gestion_ecole/acceuil">
+                <i class="fa-solid fa-house"></i>
+                <span class="menu-text">Acceuil</span>
+            </a>
+        </li>
 
+        <li>
+            <a href="/projet_gestion_ecole/connexion">
+                <i class="fa-solid fa-user"></i>
+                <span class="menu-text">Se connecter</span>
+            </a>
+        </li>
+    </ul>
 </header>
--- views/index.php
+++ views/index.php
@@ -1,44 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"
-        integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="css/index.css">
-</head>
+<?php include_once "head.php"; ?>
 
 <body>
     <?php
     include_once "header.php";
     ?>
-<section>
-    <div>
-        <img src="images/prem3.jpg" alt="image" id="photo">
-    </div>
-    <div>
-         <h1>
-            <b>Description du projet</b>
-         </h1>
-          <p>
-    L’école de la réussite est un établissement d’enseignement privé mixte (élémentaire et secondaire) avec une réputation qui l’a précède faisant d’elle une cible prioritaire à tous ceux qui souhaitent bénéficier d’une formation de qualité. 
-    Les parents d’élèves étant conscient de la situation font converger leurs enfants dans ladite école pour leur faire bénéficier d’une éducation de base leurs permettant d’asseoir au maximum leur niveau.
-     Au cours de l’année académique 2021/2022 l’effectif de l'école a dépassé les 10 000 écoliers.
-      Face à cette montée exponentielle du nombre d'inscrits, l’administration se sent véritablement débordée dans la gestion et décide de digitaliser son système.   
-         </p>
-    </div>
-   
-</section>
-<?php
-include "footer.php";
-?>
+    <section id="desc">
+        <div id="hero">
+            <img src="images/prem3.jpg" alt="image" id="photo">
+        </div>
+        <div>
+            <h1>
+                <b>Description du projet</b>
+            </h1>
+            <p>
+                L’école de la réussite est un établissement d’enseignement privé mixte (élémentaire et secondaire) avec
+                une réputation qui l’a précède faisant d’elle une cible prioritaire à tous ceux qui souhaitent
+                bénéficier d’une formation de qualité.
+                Les parents d’élèves étant conscient de la situation font converger leurs enfants dans ladite école pour
+                leur faire bénéficier d’une éducation de base leurs permettant d’asseoir au maximum leur niveau.
+                Au cours de l’année académique 2021/2022 l’effectif de l'école a dépassé les 10 000 écoliers.
+                Face à cette montée exponentielle du nombre d'inscrits, l’administration se sent véritablement débordée
+                dans la gestion et décide de digitaliser son système.
+            </p>
+        </div>
+
+    </section>
+    <?php
+    include "footer.php";
+    ?>
 
 </body>
 
