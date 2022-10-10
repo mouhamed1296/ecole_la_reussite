73600910577bc7575a323b2373bad0bf36266b76
Mamadou Sarr <mbayelel@gmail.com>
2022-10-10 18:12:24 +0000

    main

--- views/js/dashboardAdmin.js
+++
@@ -1,8 +0,0 @@
-
-const humburger = document.querySelector(".humburger");
-const navMenu  = document.querySelector(".nav_menu");
-
-humburger.addEventListener("click", () =>{
-    humburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
